4575a72b538560f266830af0de1a73d85bb0c362
Davi Amaral <40430859+daviamarall@users.noreply.github.com>
2022-12-05 21:53:40 -0300

    Add files via upload

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,8 @@
+public class TestaPontoFlutuante {
+    public static void main(String[] args) {
+
+        double salario;
+        salario = 2321.50;
+        System.out.println("meu salario é:" + salario);
+    }
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,9 @@
+public class TestaVariaveis {
+    public static void main(String[] args){
+        System.out.println("olá novo teste");
+
+        int idade;
+        idade = 21;
+        System.out.println("A idade é :" + idade + "Parabéns");
+    }
+}
